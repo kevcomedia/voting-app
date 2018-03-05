ccb7853ce71228f1cf5bd7641506ecc8d3698180
Kev Comedia <kevcomedia@gmail.com>
2018-03-05 16:45:13 +0800

    Connect to local database

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const http = require('http');
+const mongoose = require('mongoose');
 const app = require('./app');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/voting-app');
 
 const port = process.env.PORT || 3000;
 app.set('port', port);
