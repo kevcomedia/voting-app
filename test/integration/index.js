fd6e5398d49161c6a5d83a5e6c2265c54a8769d0
Kev Comedia <kevcomedia@gmail.com>
2018-03-05 14:09:22 +0800

    Connect and disconnect database on root hooks

--- test/integration/index.js
+++ test/integration/index.js
@@ -1,6 +1,8 @@
+const async = require('async');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const http = require('http');
+const mongoose = require('mongoose');
 
 const {expect} = chai;
 chai.use(chaiHttp);
@@ -8,12 +10,37 @@ chai.use(chaiHttp);
 const app = require('../../app');
 const server = http.createServer(app);
 
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/test-voting-app');
+
 before(function startServer(done) {
-  server.listen(3001, done);
+  // Using async is probably overkill, but other alternatives are promisifying
+  // both, then use Promise.all, or just nest the callbacks
+  async.parallel(
+    [
+      callback => {
+        mongoose.connection.once('connected', callback);
+      },
+      callback => {
+        server.listen(3001, callback);
+      },
+    ],
+    done
+  );
 });
 
 after(function stopServer(done) {
-  server.close(done);
+  async.parallel(
+    [
+      callback => {
+        mongoose.connection.close(callback);
+      },
+      callback => {
+        server.close(callback);
+      },
+    ],
+    done
+  );
 });
 
 module.exports = () => chai.request(server);
