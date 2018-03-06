a8b2a78f497f303f6db5c98674853bdcfc077eb9
Kev Comedia <kevcomedia@gmail.com>
2018-03-06 16:39:22 +0800

    Write global error handler

--- app.js
+++ app.js
@@ -5,8 +5,17 @@ const app = express();
 
 app.use('/', api);
 
-app.get('/', (req, res) => {
-  res.send('Hello world');
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || 'Internal Server Error',
+  });
 });
 
 module.exports = app;
