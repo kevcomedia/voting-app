713710d95aa7117d1e2abeb5f286c886a5676fbd
Kev Comedia <kevcomedia@gmail.com>
2018-03-16 17:15:55 +0800

    Implement user registration

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,5 +1,35 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/user.model');
+
 function register(req, res, next) {
-  res.status(500).send('Not implemented');
+  const {username, password} = req.body;
+  Promise.all([User.createUser({username, password}), signJwt({username})])
+    .then(([createdUser, token]) => {
+      res.status(201).json({
+        success: true,
+        message: 'User has been created',
+        username,
+        token,
+      });
+    })
+    .catch(err => {
+      err.status = 422;
+      next(err);
+    });
+}
+
+// Probably move this to some util directory. That way this can be tested.
+function signJwt(payload = {}) {
+  return new Promise(function(resolve, reject) {
+    jwt.sign(
+      payload,
+      process.env.JWT_SECRET_KEY || 'use a proper key',
+      (err, token) => {
+        if (err) return reject(err);
+        resolve(token);
+      }
+    );
+  });
 }
 
 module.exports = {
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const authController = require('../controllers/auth.controller');
 
 const authRouter = express.Router();
+
+authRouter.use(bodyParser.json());
+authRouter.use(bodyParser.urlencoded({extended: false}));
 
 authRouter.post('/register', authController.register);
 
