50554913392be1c84c2760d4ec65dabbdf2e17c1
Kev Comedia <kevcomedia@gmail.com>
2018-03-16 17:05:34 +0800

    Condense registration tests

--- test/integration/auth/register.test.js
+++ test/integration/auth/register.test.js
@@ -13,157 +13,87 @@ describe('User registration', function() {
   before(dropUsers);
   afterEach(dropUsers);
 
-  describe('Successful registration', function() {
-    let response;
-
-    before(function(done) {
-      request()
-        .post(registerEndpoint)
-        .send({username: 'newuser', password: 'new-password'})
-        .end((err, res) => {
-          response = res;
-          done();
-        });
-    });
-
-    it('has status code 201', function() {
-      expect(response).to.have.status(201);
-    });
-
-    it('should be a JSON', function() {
-      expect(response).to.be.a.json;
-    });
-
-    it('contains a `success` property set to `true`', function() {
-      expect(response.body)
-        .to.have.property('success')
-        .that.equals(true);
-    });
-
-    it('contains the username', function() {
-      expect(response.body)
-        .to.have.property('username')
-        .that.equals('newuser');
-    });
-
-    it('contains the JWT', function() {
-      expect(response.body)
-        .to.have.property('token')
-        .that.matches(/^[0-9A-Za-z\-_]+\.[0-9A-Za-z\-_]+\.[0-9A-Za-z\-_]+/);
-    });
+  it('registers successfully', function(done) {
+    request()
+      .post(registerEndpoint)
+      .send({username: 'newuser', password: 'new-password'})
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        expect(res).to.be.a.json;
+        expect(res.body)
+          .to.have.property('success')
+          .that.equals(true);
+        expect(res.body)
+          .to.have.property('username')
+          .that.equals('newuser');
+        expect(res.body)
+          .to.have.property('token')
+          .that.matches(/^[0-9A-Za-z\-_]+\.[0-9A-Za-z\-_]+\.[0-9A-Za-z\-_]+/);
+        done();
+      });
   });
 
-  describe('Duplicate username', function() {
-    let error;
-
-    before(function(done) {
-      async.series(
-        [
-          callback => {
-            request()
-              .post(registerEndpoint)
-              .send({username: 'newuser', password: 'new-password'})
-              .end(callback);
-          },
-          callback => {
-            request()
-              .post(registerEndpoint)
-              .send({username: 'newuser', password: 'new-password'})
-              .end((err, res) => {
-                error = err;
-              });
-          },
-        ],
-        done
-      );
-    });
-
-    it('has status code 422', function() {
-      expect(error).to.have.status(422);
-    });
-
-    it('should be a JSON', function() {
-      expect(error).to.be.a.json;
-    });
-
-    it('contains a `success` property set to `false`', function() {
-      expect(error.body)
-        .to.have.property('success')
-        .that.equals('false');
-    });
-
-    it('contains a "Username taken" message', function() {
-      expect(error.body)
-        .to.have.property('message')
-        .that.equals('Username is already taken');
-    });
+  it("doesn't register duplicate usernames", function(done) {
+    async.waterfall(
+      [
+        callback => {
+          request()
+            .post(registerEndpoint)
+            .send({username: 'newuser', password: 'new-password'})
+            .end(callback);
+        },
+        (err, res, callback) => {
+          request()
+            .post(registerEndpoint)
+            .send({username: 'newuser', password: 'new-password'})
+            .end(callback);
+        },
+      ],
+      (err, res) => {
+        expect(err).to.have.status(422);
+        expect(err).to.be.a.json;
+        expect(err.body)
+          .to.have.property('success')
+          .that.equals('false');
+        expect(err.body)
+          .to.have.property('message')
+          .that.equals('Username is already taken');
+        done();
+      }
+    );
   });
 
-  describe('Password less than 10 characters', function() {
-    let error;
-
-    before(function(done) {
-      request()
-        .post(registerEndpoint)
-        .send({username: 'newUser', password: '123456789'})
-        .end((err, res) => {
-          error = err;
-          done();
-        });
-    });
-
-    it('has status code 422', function() {
-      expect(error).to.have.status(422);
-    });
-
-    it('should be a JSON', function() {
-      expect(error).to.be.a.json;
-    });
-
-    it('contains a `success` property set to `false`', function() {
-      expect(error.body)
-        .to.have.property('success')
-        .that.equals('false');
-    });
-
-    it('contains a "Password is too short" message', function() {
-      expect(error.body)
-        .to.have.property('message')
-        .that.equals('Password should be at least 10 characters long');
-    });
+  it("doesn't register if password is too short", function(done) {
+    request()
+      .post(registerEndpoint)
+      .send({username: 'newUser', password: '123456789'})
+      .end((err, res) => {
+        expect(err).to.have.status(422);
+        expect(err).to.be.a.json;
+        expect(err.body)
+          .to.have.property('success')
+          .that.equals('false');
+        expect(err.body)
+          .to.have.property('message')
+          .that.equals('Password should be at least 10 characters long');
+        done();
+      });
   });
 
-  describe('Username is blank', function() {
-    let error;
-
-    before(function(done) {
-      request()
-        .post(registerEndpoint)
-        .send({username: '', password: 'new-password'})
-        .end((err, res) => {
-          error = err;
-          done();
-        });
-    });
-
-    it('has status code 422', function() {
-      expect(error).to.have.status(422);
-    });
-
-    it('should be a JSON', function() {
-      expect(error).to.be.a.json;
-    });
-
-    it('contains a `success` property set to `false`', function() {
-      expect(error.body)
-        .to.have.property('success')
-        .that.equals('false');
-    });
-
-    it('contains a "Blank username" message', function() {
-      expect(error.body)
-        .to.have.property('message')
-        .that.equals('Username cannot be blank');
-    });
+  it("doesn't register if the username is blank", function(done) {
+    request()
+      .post(registerEndpoint)
+      .send({username: '', password: 'new-password'})
+      .end((err, res) => {
+        expect(err).to.have.status(422);
+        expect(err).to.be.a.json;
+        expect(err.body)
+          .to.have.property('success')
+          .that.equals('false');
+        expect(err.body)
+          .to.have.property('message')
+          .that.equals('Username cannot be blank');
+        done();
+      });
   });
 });
