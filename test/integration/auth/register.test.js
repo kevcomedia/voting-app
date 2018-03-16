fc9a78115bee4f34f31ce5b96ab0620db78d759b
Kev Comedia <kevcomedia@gmail.com>
2018-03-16 18:01:06 +0800

    Rewrite failing tests
    For 4xx responses, the response is embedded in the error as a `response`
    property.

--- test/integration/auth/register.test.js
+++ test/integration/auth/register.test.js
@@ -1,4 +1,3 @@
-const async = require('async');
 const {expect} = require('chai');
 const request = require('..');
 const User = require('../../../models/user.model');
@@ -34,46 +33,39 @@ describe('User registration', function() {
   });
 
   it("doesn't register duplicate usernames", function(done) {
-    async.waterfall(
-      [
-        callback => {
-          request()
-            .post(registerEndpoint)
-            .send({username: 'newuser', password: 'new-password'})
-            .end(callback);
-        },
-        (err, res, callback) => {
-          request()
-            .post(registerEndpoint)
-            .send({username: 'newuser', password: 'new-password'})
-            .end(callback);
-        },
-      ],
-      (err, res) => {
-        expect(err).to.have.status(422);
-        expect(err).to.be.a.json;
-        expect(err.body)
+    request()
+      .post(registerEndpoint)
+      .send({username: 'newuser', password: 'new-password'})
+      .then(() =>
+        request()
+          .post(registerEndpoint)
+          .send({username: 'newuser', password: 'new-password'})
+      )
+      .catch(({response}) => {
+        expect(response).to.have.status(422);
+        expect(response).to.be.a.json;
+        expect(response.body)
           .to.have.property('success')
-          .that.equals('false');
-        expect(err.body)
+          .that.equals(false);
+        expect(response.body)
           .to.have.property('message')
           .that.equals('Username is already taken');
         done();
-      }
-    );
+      })
+      .catch(done);
   });
 
   it("doesn't register if password is too short", function(done) {
     request()
       .post(registerEndpoint)
       .send({username: 'newUser', password: '123456789'})
-      .end((err, res) => {
-        expect(err).to.have.status(422);
-        expect(err).to.be.a.json;
-        expect(err.body)
+      .end(({response}) => {
+        expect(response).to.have.status(422);
+        expect(response).to.be.a.json;
+        expect(response.body)
           .to.have.property('success')
-          .that.equals('false');
-        expect(err.body)
+          .that.equals(false);
+        expect(response.body)
           .to.have.property('message')
           .that.equals('Password should be at least 10 characters long');
         done();
@@ -84,13 +76,13 @@ describe('User registration', function() {
     request()
       .post(registerEndpoint)
       .send({username: '', password: 'new-password'})
-      .end((err, res) => {
-        expect(err).to.have.status(422);
-        expect(err).to.be.a.json;
-        expect(err.body)
+      .end(({response}) => {
+        expect(response).to.have.status(422);
+        expect(response).to.be.a.json;
+        expect(response.body)
           .to.have.property('success')
-          .that.equals('false');
-        expect(err.body)
+          .that.equals(false);
+        expect(response.body)
           .to.have.property('message')
           .that.equals('Username cannot be blank');
         done();
