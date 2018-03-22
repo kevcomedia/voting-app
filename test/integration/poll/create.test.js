58c2fdd71e422cb4aad6e660ee091213ad526968
Kev Comedia <kevcomedia@gmail.com>
2018-03-22 13:48:33 +0800

    Write poll creation tests

--- test/integration/poll/create.test.js
+++ test/integration/poll/create.test.js
@@ -1,24 +1,167 @@
 const {expect} = require('chai');
 const request = require('..');
 const Poll = require('../../../models/poll.model');
+const User = require('../../../models/user.model');
+
+const endpoint = '/api/v1/polls/create';
 
 describe('Poll creation', function() {
-  const endpoint = '/api/v1/polls/create';
+  let token;
+
+  const createPollRequest = () =>
+    request()
+      .post(endpoint)
+      .set('Authorization', `Bearer ${token}`);
+
+  before(function createToken(done) {
+    // Probably a more sophisticated approach is to stub the parts that
+    // accept/generate a token
+    request()
+      .post('/api/v1/register')
+      .send({
+        username: 'sample-user',
+        password: 'sample-password',
+      })
+      .then(res => {
+        token = res.body.token;
+        done();
+      })
+      .catch(done);
+  });
+
+  after(function removeCreatedUser(done) {
+    User.remove({}, done);
+  });
 
   describe('Polls with at least two choices', function() {
-    it('creates a poll with two choices');
-    it('creates a poll with three choices');
+    const cases = [
+      {
+        label: 'creates a poll with two choices',
+        body: {
+          question: 'Example poll with two choices',
+          choices: ['Choice 1', 'Choice 2'],
+        },
+      },
+      {
+        label: 'creates a poll with three choices',
+        body: {
+          question: 'Example poll with three choices',
+          choices: ['Choice 1', 'Choice 2', 'Choice 3'],
+        },
+      },
+    ];
+
+    cases.forEach(({label, body}) => {
+      it(label, function(done) {
+        createPollRequest()
+          .send(body)
+          .then(res => {
+            expect(res).to.have.status(201);
+            expect(res).to.be.a.json;
+            expect(res.body).to.have.property('success').that.is.true;
+            expect(res.body)
+              .to.have.property('message')
+              .that.equals('Poll created');
+            expect(res.body)
+              .to.have.property('poll')
+              .that.equals(body);
+            done();
+          })
+          .catch(done);
+      });
+    });
   });
 
   describe('Polls with less than two choices', function() {
-    it("doesn't create a poll with just one choice");
-    it("doesn't create a poll with no choices");
+    const cases = [
+      {
+        label: "doesn't create a poll with just one choice",
+        body: {
+          question: 'Example poll with one choice',
+          choices: ['Choice 1'],
+        },
+      },
+      {
+        label: "doesn't create a poll with no choices",
+        body: {
+          question: 'Example poll with no choices',
+          choices: [],
+        },
+      },
+    ];
+
+    cases.forEach(({label, body}) => {
+      it(label, function(done) {
+        createPollRequest()
+          .send(body)
+          .catch(({response}) => {
+            expect(response).to.have.status(422);
+            expect(response).to.be.a.json;
+            expect(response.body).to.have.property('success').that.is.false;
+            expect(response.body)
+              .to.have.property('message')
+              .that.equals('There should be at least two choices');
+            done();
+          })
+          .catch(done);
+      });
+    });
   });
 
-  it("doesn't create a poll with no question");
-  it("doesn't create a poll with a blank choice");
+  it("doesn't create a poll with no question", function(done) {
+    createPollRequest()
+      .send({
+        question: '', // or null? or just whitespace?
+        choices: ['Choice 1', 'Choice 2'],
+      })
+      .catch(({response}) => {
+        expect(response).to.have.status(422);
+        expect(response).to.be.a.json;
+        expect(response.body).to.have.property('success').that.is.false;
+        expect(response.body)
+          .to.have.property('message')
+          .that.equals('There should be a question');
+        done();
+      })
+      .catch(done);
+  });
+
+  it("doesn't create a poll with a blank choice", function(done) {
+    createPollRequest()
+      .send({
+        question: 'Example poll with missing choice',
+        choices: ['', 'Choice 2'],
+      })
+      .catch(({response}) => {
+        expect(response).to.have.status(422);
+        expect(response).to.be.a.json;
+        expect(response.body).to.have.property('success').that.is.false;
+        expect(response.body)
+          .to.have.property('message')
+          .that.equals('Choices cannot be blank');
+        done();
+      })
+      .catch(done);
+  });
 });
 
 describe('Poll creation (unauthenticated user)', function() {
-  it('should not create a poll');
+  it('should not create a poll', function(done) {
+    request()
+      .post(endpoint)
+      .send({
+        question: 'Example poll that should not be created',
+        choices: ['Choice 1', 'Choice 2'],
+      })
+      .catch(({response}) => {
+        expect(response).to.have.status(401);
+        expect(response).to.be.a.json;
+        expect(response.body).to.have.property('success').that.is.false;
+        expect(response.body)
+          .to.have.property('message')
+          .that.equals('Unauthorized');
+        done();
+      })
+      .catch(done);
+  });
 });
