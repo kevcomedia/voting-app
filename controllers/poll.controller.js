1d08da5be6f05d3ee784478fc843bda68aada1c1
Kev Comedia <kevcomedia@gmail.com>
2018-03-23 18:58:01 +0800

    Validate poll creation with express validator

--- controllers/poll.controller.js
+++ controllers/poll.controller.js
@@ -1,41 +1,54 @@
+const {check, validationResult} = require('express-validator/check');
+const {matchedData, sanitize} = require('express-validator/filter');
 const Poll = require('../models/poll.model');
 
-function create(req, res, next) {
-  const {question, choices} = req.body;
+const create = [
+  [
+    check('question')
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage('There should be a question'),
+    check('choices')
+      .custom(Array.isArray)
+      .withMessage('There should be at least two choices'),
+    check('choices')
+      .custom(choices => choices.length >= 2)
+      .withMessage('There should be at least two choices'),
+    check('choices.*')
+      .trim()
+      .not()
+      .isEmpty()
+      .withMessage('Choices cannot be blank'),
+  ],
+  function create(req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const err = new Error(errors.array()[0].msg);
+      err.status = 422;
+      return next(err);
+    }
 
-  if (!question) {
-    const err = new Error('There should be a question');
-    err.status = 422;
-    return next(err);
-  }
+    const {question, choices} = matchedData(req);
 
-  if (!choices || choices.length < 2) {
-    const err = new Error('There should be at least two choices');
-    err.status = 422;
-    return next(err);
-  }
-
-  if (choices.some(choice => !choice)) {
-    const err = new Error('Choices cannot be blank');
-    err.status = 422;
-    return next(err);
-  }
-
-  Poll.create({
-    question,
-    choices: choices.map(choice => ({label: choice})),
-    meta: {owner: req.user.id},
-  })
-    .then(poll => Poll.populate(poll, {path: 'meta.owner', select: 'username'}))
-    .then(poll => {
-      res.status(201).json({
-        success: true,
-        message: 'Poll created',
-        poll,
-      });
+    Poll.create({
+      question,
+      choices: choices.map(choice => ({label: choice})),
+      meta: {owner: req.user.id},
     })
-    .catch(next);
-}
+      .then(poll =>
+        Poll.populate(poll, {path: 'meta.owner', select: 'username'})
+      )
+      .then(poll => {
+        res.status(201).json({
+          success: true,
+          message: 'Poll created',
+          poll,
+        });
+      })
+      .catch(next);
+  },
+];
 
 module.exports = {
   create,
